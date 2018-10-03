d1b0996c55472ac1264b9e8e8bd246801f179697
José Luis R <joseluisruizoliver@gmail.com>
2018-10-03 12:40:34 +0200

    Click en los fragments
    añadido clicks en los fragments para que cambien y un savedInstanceState para el giro de la pantalla

--- Android_Me-TFragments.00-StartingCode/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ Android_Me-TFragments.00-StartingCode/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -11,10 +11,13 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BodyPartFragment extends Fragment {
 
+    public static final String IMAGE_ID="image_id";
+    public static final String IMAGE_LIST="image_list";
     private List<Integer> list;
     private int id;
 
@@ -26,14 +29,27 @@ public class BodyPartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
+        if(savedInstanceState != null){
+            id=savedInstanceState.getInt(IMAGE_ID);
+            list=savedInstanceState.getIntegerArrayList(IMAGE_LIST);
+        }
 
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
 
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
         if(imageView != null && list!= null)
             imageView.setImageResource(list.get(id));
 
+        imageView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if(id < list.size() - 1)
+                    id++;
+                else
+                    id=0;
+                imageView.setImageResource(list.get(id));
+            }
+        });
         return rootView;
     }
 
@@ -43,5 +59,10 @@ public class BodyPartFragment extends Fragment {
 
     public void setId(int id){
         this.id=id;
+    }
+
+    public void onSaveInstanceState(Bundle currentState){
+        currentState.putIntegerArrayList(IMAGE_LIST,(ArrayList<Integer>) list);
+        currentState.putInt(IMAGE_ID,id);
     }
 }
